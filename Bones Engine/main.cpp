b670046c50b53b97d793131ba7f29b637130bab6
HarpreetBains <happiebains@yahoo.com>
2019-10-13 01:24:11 -0700

    added GUI to the project

--- Bones Engine/main.cpp
+++ Bones Engine/main.cpp
@@ -1,11 +1,17 @@
 #include "main.h"
 
-int main() {
-	Game game;
+int main(int argc, char** argv)
+{
+	Game* game = new Game(argc, argv);
+	
 	bool success = game.Initialize();
-	if (success) {
+	if (success)
+	{
 		game.RunLoop();
 	}
+	
 	game.Shutdown();
+	delete game;
+
 	return 0;
 }
